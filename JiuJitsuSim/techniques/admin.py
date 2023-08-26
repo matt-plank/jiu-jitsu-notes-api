d2ff740e98ee5fcfe11635ce32b565ce4340b48d
matt-plank <matthew_plank@outlook.com>
2023-08-26 13:02:11 +0100

    Tidied up positions page on admin dashboard

--- JiuJitsuSim/techniques/admin.py
+++ JiuJitsuSim/techniques/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from .models import Grip, Position, Technique
+from .models import Grip, Position, Technique, grip_names_list
 
-admin.site.register(Position)
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name", "aspect", "your_grips_show", "their_grips_show")
+    ordering = ("name", "aspect")
+    search_fields = ("name", "aspect")
+
+    def your_grips_show(self, obj) -> str:
+        return grip_names_list(obj.your_grips.all())
+
+    def their_grips_show(self, obj) -> str:
+        return grip_names_list(obj.their_grips.all())
+
+
 admin.site.register(Technique)
 admin.site.register(Grip)
