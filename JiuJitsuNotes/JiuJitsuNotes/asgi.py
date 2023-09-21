c202f224a366df73cff06f0f35a41b5ff5b0c07e
matt-plank <matthew_plank@outlook.com>
2023-09-21 11:57:45 +0100

    Renamed app from JiuJitsuSim to JiuJitsuNotes

--- JiuJitsuNotes/JiuJitsuNotes/asgi.py
+++ JiuJitsuNotes/JiuJitsuNotes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for JiuJitsuSim project.
+ASGI config for JiuJitsuNotes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JiuJitsuSim.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "JiuJitsuNotes.settings")
 
 application = get_asgi_application()
