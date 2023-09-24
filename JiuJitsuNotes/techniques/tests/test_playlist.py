32978afe7793ecf53e469723f738f2900f1d3ff9
matt-plank <matthew_plank@outlook.com>
2023-09-24 20:26:22 +0100

    Added PUT POST DELETE /api/playlists/

--- JiuJitsuNotes/techniques/tests/test_playlist.py
+++ JiuJitsuNotes/techniques/tests/test_playlist.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from ..models import Playlist
+
 
 class TestPlaylist(TestCase):
     fixtures = [
@@ -69,3 +71,90 @@ class TestPlaylist(TestCase):
                 ],
             },
         )
+
+    def test_put(self):
+        response = self.client.put(
+            "/api/playlists/",
+            data={
+                "id": 1,
+                "name": "Shoulder Crunchy",
+            },
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "id": 1,
+                "name": "Shoulder Crunchy",
+                "description": "Shoulder crunch playlist",
+                "positions": [
+                    {
+                        "id": 2,
+                        "aspect": "Playing",
+                        "name": "Closed Guard",
+                        "display_name": "Playing Closed Guard (Shoulder Crunch)",
+                        "your_grips": [
+                            {
+                                "id": 2,
+                                "name": "Shoulder Crunch",
+                            }
+                        ],
+                        "their_grips": [],
+                    },
+                ],
+            },
+        )
+
+    def test_post(self):
+        response = self.client.post(
+            "/api/playlists/",
+            data={
+                "name": "Shoulder Crunchyyy",
+                "description": "Another sholder crunch playlist",
+                "positions": [
+                    {
+                        "id": 2,
+                    }
+                ],
+            },
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(
+            response.json(),
+            {
+                "id": 2,
+                "name": "Shoulder Crunchyyy",
+                "description": "Another sholder crunch playlist",
+                "positions": [
+                    {
+                        "id": 2,
+                        "aspect": "Playing",
+                        "name": "Closed Guard",
+                        "display_name": "Playing Closed Guard (Shoulder Crunch)",
+                        "your_grips": [
+                            {
+                                "id": 2,
+                                "name": "Shoulder Crunch",
+                            }
+                        ],
+                        "their_grips": [],
+                    },
+                ],
+            },
+        )
+
+    def test_delete(self):
+        response = self.client.delete(
+            "/api/playlists/",
+            data={
+                "id": 1,
+            },
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Playlist.objects.count(), 0)
--- JiuJitsuNotes/techniques/views/playlist.py
+++ JiuJitsuNotes/techniques/views/playlist.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView, Response
 
-from .. import models
+from ..models import Playlist, Position
 from ..serializers import playlist as playlist_serializers
 
 
@@ -10,7 +10,7 @@ class PlaylistView(APIView):
     def get_single(self, request):
         """GET a single playlist."""
         id: int = request.query_params["id"]
-        playlist: models.Playlist = models.Playlist.objects.get(pk=id)
+        playlist: Playlist = Playlist.objects.get(pk=id)
 
         result = playlist_serializers.CompleteSerializer(playlist).data
 
@@ -18,7 +18,7 @@ class PlaylistView(APIView):
 
     def get_many(self, request):
         """GET all playlists."""
-        playlists = models.Playlist.objects.all()
+        playlists = Playlist.objects.all()
         result = playlist_serializers.CompleteSerializer(playlists, many=True).data
 
         return Response(result)
@@ -29,3 +29,59 @@ class PlaylistView(APIView):
             return self.get_single(request)
 
         return self.get_many(request)
+
+    def put(self, request):
+        """Handle PUT request. Update an existing playlist."""
+        if "id" not in request.data:
+            return Response({"error": "id not provided"}, status=400)
+
+        playlist: Playlist = Playlist.objects.get(pk=request.data["id"])
+
+        if "name" in request.data:
+            playlist.name = request.data["name"]
+
+        if "description" in request.data:
+            playlist.description = request.data["description"]
+
+        if "positions" in request.data:
+            playlist.positions.clear()
+            for position_data in request.data["positions"]:
+                position_id: int = position_data["id"]
+                position: Position = Position.objects.get(pk=position_id)
+                playlist.positions.add(position)
+
+        result = playlist_serializers.CompleteSerializer(playlist).data
+
+        return Response(result)
+
+    def post(self, request):
+        """Handle POST request. Create a new playlist."""
+        if "name" not in request.data:
+            return Response({"error": "name not provided"}, status=400)
+
+        if "description" not in request.data:
+            return Response({"error": "description not provided"}, status=400)
+
+        playlist: Playlist = Playlist.objects.create(
+            name=request.data["name"],
+            description=request.data["description"],
+        )
+
+        for position_data in request.data.get("positions", []):
+            position_id: int = position_data["id"]
+            position: Position = Position.objects.get(pk=position_id)
+            playlist.positions.add(position)
+
+        result = playlist_serializers.CompleteSerializer(playlist).data
+
+        return Response(result)
+
+    def delete(self, request):
+        """Handle DELETE request. Delete a playlist."""
+        if "id" not in request.data:
+            return Response({"error": "id not provided"}, status=400)
+
+        playlist: Playlist = Playlist.objects.get(pk=request.data["id"])
+        playlist.delete()
+
+        return Response(status=200)
