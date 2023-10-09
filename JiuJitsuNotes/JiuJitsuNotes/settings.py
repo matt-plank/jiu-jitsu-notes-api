9d505c8f839d8c8ea1c3ec96f18ff9f2114a966e
matt-plank <matthew_plank@outlook.com>
2023-10-09 15:18:53 +0100

    Added static files configuration

--- JiuJitsuNotes/JiuJitsuNotes/settings.py
+++ JiuJitsuNotes/JiuJitsuNotes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +137,5 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
